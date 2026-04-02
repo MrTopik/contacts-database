fe2557481df9211bfc528372fb1d190f2f113811
MrTopik <topraktarikk@outlook.com>
2026-04-02 23:05:00 +0300

    Updated Add and update
    Update now shows old data inside the database
    For Update and Add a new window opens and the main window stays

--- main/addWindow.py
+++ main/addWindow.py
@@ -15,49 +15,47 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFormLayout, QLabel, QLineEdit,
-    QMainWindow, QMenuBar, QPushButton, QSizePolicy,
-    QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QDialog, QFormLayout, QLabel,
+    QLineEdit, QPushButton, QSizePolicy, QVBoxLayout,
+    QWidget)
 
-class AddDialog(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(519, 227)
-        self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(400, 300)
+        self.verticalLayout_2 = QVBoxLayout(Dialog)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.formLayout = QFormLayout()
         self.formLayout.setObjectName(u"formLayout")
-        self.nameEntry = QLineEdit(self.centralwidget)
+        self.nameEntry = QLineEdit(Dialog)
         self.nameEntry.setObjectName(u"nameEntry")
 
         self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.nameEntry)
 
-        self.label_2 = QLabel(self.centralwidget)
+        self.label_2 = QLabel(Dialog)
         self.label_2.setObjectName(u"label_2")
 
         self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.label_2)
 
-        self.phoneEntry = QLineEdit(self.centralwidget)
+        self.phoneEntry = QLineEdit(Dialog)
         self.phoneEntry.setObjectName(u"phoneEntry")
 
         self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.phoneEntry)
 
-        self.label_3 = QLabel(self.centralwidget)
+        self.label_3 = QLabel(Dialog)
         self.label_3.setObjectName(u"label_3")
 
         self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_3)
 
-        self.mailEntry = QLineEdit(self.centralwidget)
+        self.mailEntry = QLineEdit(Dialog)
         self.mailEntry.setObjectName(u"mailEntry")
 
         self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.mailEntry)
 
-        self.label = QLabel(self.centralwidget)
+        self.label = QLabel(Dialog)
         self.label.setObjectName(u"label")
 
         self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label)
@@ -65,7 +63,7 @@ class AddDialog(object):
 
         self.verticalLayout.addLayout(self.formLayout)
 
-        self.addConfirm = QPushButton(self.centralwidget)
+        self.addConfirm = QPushButton(Dialog)
         self.addConfirm.setObjectName(u"addConfirm")
 
         self.verticalLayout.addWidget(self.addConfirm)
@@ -73,25 +71,17 @@ class AddDialog(object):
 
         self.verticalLayout_2.addLayout(self.verticalLayout)
 
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QMenuBar(MainWindow)
-        self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 519, 30))
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QStatusBar(MainWindow)
-        self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi(Dialog)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(Dialog)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Data Window", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Phone:", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"E-Mail:", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Name:", None))
-        self.addConfirm.setText(QCoreApplication.translate("MainWindow", u"Add", None))
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Dialog", None))
+        self.label_2.setText(QCoreApplication.translate("Dialog", u"Phone:", None))
+        self.label_3.setText(QCoreApplication.translate("Dialog", u"E-Mail:", None))
+        self.label.setText(QCoreApplication.translate("Dialog", u"Name:", None))
+        self.addConfirm.setText(QCoreApplication.translate("Dialog", u"Add", None))
     # retranslateUi
 
--- main/main.py
+++ main/main.py
@@ -1,19 +1,31 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHeaderView
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QHeaderView, QDialog
 from basicUI import Ui_MainWindow
-from addWindow import AddDialog
+from addWindow import Ui_Dialog
 from loginUI import Ui_loginUI
 from database import add_contact, get_all_contacts, delete_contact, update_contact, setup
 
 setup() #Database setup
 
+class AddDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.ui.addConfirm.clicked.connect(self.accept)
+
+    def get_data(self):
+        return (self.ui.nameEntry.text(),self.ui.phoneEntry.text(),self.ui.mailEntry.text())
+    def set_data(self, name, phone, mail):
+        self.ui.nameEntry.setText(name)
+        self.ui.phoneEntry.setText(phone)
+        self.ui.mailEntry.setText(mail)
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
-        self.add = AddDialog()
         self.logui = Ui_loginUI()
-
         self.logui.setupUi(self)
         self.logui.loginButton.clicked.connect(self.login)
 
@@ -40,23 +52,30 @@ class MainWindow(QMainWindow):
             
     #Database functions
     def add_entry(self):
-        self.add.setupUi(self)
-        self.add.addConfirm.clicked.connect(self.contactAddConfirm)
-        
-    def contactAddConfirm(self):
-        add_contact(self.add.nameEntry.text(),self.add.phoneEntry.text(),self.add.mailEntry.text())
-        self.UIsetup()
+        dialog = AddDialog(self)
+        if dialog.exec() == QDialog.Accepted:
+            name, phone, mail = dialog.get_data()
+            add_contact(name,phone,mail)
+            self.UIsetup()
 
     def update(self):
         selected = self.ui.tableWidget.selectedItems()
-        self.rowUp = self.ui.tableWidget.row(selected[0])
-        self.add.setupUi(self)
-        self.add.addConfirm.setText("Update")
-        self.add.addConfirm.clicked.connect(self.updateConfirm)
-    
-    def updateConfirm(self):
-        update_contact(self.rowUp+1,self.add.nameEntry.text(),self.add.phoneEntry.text(),self.add.mailEntry.text())
-        self.UIsetup()
+        if not selected:
+            return
+        row = self.ui.tableWidget.row(selected[0])
+
+        name  = self.ui.tableWidget.item(row, 0).text()
+        phone = self.ui.tableWidget.item(row, 1).text()
+        mail  = self.ui.tableWidget.item(row, 2).text()
+
+        dialog = AddDialog(self)          # Reuse the same dialog for updates
+        dialog.set_data(name, phone, mail)
+        dialog.ui.addConfirm.setText("Update")
+
+        if dialog.exec() == QDialog.Accepted:
+            new_name, new_phone, new_mail = dialog.get_data()
+            update_contact(row + 1, new_name, new_phone, new_mail)
+            self.UIsetup()
 
     def remove_entry(self):
         selected = self.ui.tableWidget.selectedItems()
